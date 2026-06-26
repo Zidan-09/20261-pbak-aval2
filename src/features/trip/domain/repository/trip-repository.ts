30d904221281111c0b4fbe3d63e65c16a53b312c
Zidan-09 <samuel.profissional18@gmail.com>
2026-06-26 09:19:09 -0300

    feat: implemented get-trip-requests use case

--- src/features/trip/domain/repository/trip-repository.ts
+++ src/features/trip/domain/repository/trip-repository.ts
@@ -1,6 +1,7 @@
 import { FindTrip } from "./find-trip";
+import { GetTrips } from "./get-trips";
 import { SaveTrip } from "./save-trip";
 
-export interface TripRepository extends SaveTrip, FindTrip {
+export interface TripRepository extends SaveTrip, FindTrip, GetTrips {
 
 }
