7581037c419ee7802e65ceb7b8b850b4ba8cb558
Erick-VP <erickvscletal31@gmail.com>
2026-06-28 16:31:50 -0300

    feat: register success handler in fastify buildApp

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import Fastify from "fastify";
+import { errorHandler } from "./shared/http/error-handler";
+import { successHandler } from "./shared/http/success-handler";
 
 export function buildApp() {
-    const app = Fastify({
-        logger: true
-    });
+  const app = Fastify({
+    logger: true,
+  });
+  app.setErrorHandler(errorHandler);
+  app.addHook("preSerialization", successHandler);
 
-    app.get("/health", async () => {
-        return {
-            success: true,
-            data: {
-                status: "ok"
-            }
-        }
-    });
+  app.get("/health", async () => {
+    return {
+      data: {
+        status: "ok",
+      },
+    };
+  });
 
-    return app;
+  return app;
 }
