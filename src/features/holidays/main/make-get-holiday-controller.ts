a32966811d995a2e7cd05c19ebaf1828d54a1052
Zidan-09 <samuel.profissional18@gmail.com>
2026-06-28 21:09:16 -0300

    fix: fixed controller assembler with the new repository to instance the Use Case

--- src/features/holidays/main/make-get-holiday-controller.ts
+++ src/features/holidays/main/make-get-holiday-controller.ts
@@ -3,13 +3,16 @@ import { BrasilApiHolidayProvider } from "../infrastructure/gateway/brasil-api/b
 import { GetHolidaysUseCase } from "../application/use-cases/get-holidays";
 import { GetHolidaysController } from "../infrastructure/controller/get-holidays-controller";
 import { prisma } from "#/shared/infrastructure/persistence/prisma/prisma";
+import { PrismaHolidaySyncRepository } from "../infrastructure/persistence/prisma/adapter/prisma-holiday-sync-repository";
 
 export function makeGetHolidayController() {
     const repository = new PrismaHolidayRepository(prisma);
+    const syncRepository = new PrismaHolidaySyncRepository(prisma);
     const provider = new BrasilApiHolidayProvider();
 
     const useCase = new GetHolidaysUseCase(
         repository,
+        syncRepository,
         provider
     );
 
